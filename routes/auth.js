ef25331e4b7dea05d7d5d6a6bc3317f8400c07a6
Samarth <samarthchouksey18@gmail.com>
2024-07-07 00:28:20 +0530

    Update auth.js - existing user check and add new users to db

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import {Strategy as GoogleStrategy} from "passport-google-oauth20";
 import passport from "passport";
 import dotenv from "dotenv";
+import db from "../routes/db.js";
 
 dotenv.config();
 const router = express.Router();
@@ -14,9 +15,33 @@ passport.use(new GoogleStrategy({
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/redirect"
   },
-  function(accessToken, refreshToken, profile, cb) {
-    // console.log(profile);
-    return cb(null, profile);
+  async function(accessToken, refreshToken, profile, cb) {
+    const firstName = profile.name.givenName;
+    const lastName = profile.name.familyName;
+    const email = profile.emails[0].value;
+    const photoLink = profile.photos[0].value;
+    const password = "google";
+    console.log(firstName, lastName, email);
+    //try-catch block for checking user existence
+    try {
+        const response = await db.query("SELECT * FROM users WHERE email=$1", [email]);
+        if (response.rowCount > 0) {
+            //user exist in db
+            return cb(null, profile);
+        } else {
+            //user not exist in db --> add user to db
+            //try-catch block for adding new user in DB
+            try {
+                await db.query(`INSERT INTO users (email, password, first_name, last_name, photo_link) 
+                VALUES ($1, $2, $3, $4, $5)`, [email, password, firstName, lastName, photoLink]);
+                return cb(null, profile);
+            } catch(err) {
+                console.log("Error adding new user to DB", err);
+            } 
+        }
+    } catch(err) {
+        console.error("Error checking user existence in DB", err);
+    } 
   }
 ));
 
